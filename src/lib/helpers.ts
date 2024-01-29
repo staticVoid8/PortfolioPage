3aa99924bbde9de1c3f19ac98cfe7241a9e95aeb
MarcelN20 <88276296+MarcelN20@users.noreply.github.com>
2024-01-29 16:25:16 +0100

    Adjust timezone

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -2,37 +2,34 @@ export function trimText(input: string, maxLength: number = 100): string {
   if (input.length <= maxLength) return input;
   return input.substring(0, maxLength - 3) + "...";
 }
-export function getCurrentTimeInItaly(): Date {
-  // Create a date object with the current UTC time
+export function getCurrentTimeGermany(): Date {
+  // neues Objekt "Date" initialisieren
   const now = new Date();
 
-  // Convert the UTC time to Italy's time
-  const offsetItaly = 2; // Italy is in Central European Summer Time (UTC+2), but you might need to adjust this based on Daylight Saving Time
-  now.setHours(now.getUTCHours() + offsetItaly);
+  // UTC in CET konvertieren
+  const offsetGermany = 2; // Central Eastern Time +2Std. offset
+  now.setHours(now.getUTCHours() + offsetGermany);
 
   return now;
 }
 
-export function formatTimeForItaly(date: Date): string {
+export function formatTimeGermany(date: Date): string {
   const options: Intl.DateTimeFormatOptions = {
     hour: "numeric",
     minute: "2-digit",
     second: "2-digit",
-    hour12: true, // This will format the time in 12-hour format with AM/PM
-    timeZone: "Europe/Rome",
+    timeZone: "Europe/Berlin",
   };
 
-  let formattedTime = new Intl.DateTimeFormat("en-US", options).format(date);
+  let formattedTime = new Intl.DateTimeFormat("de", options).format(date);
 
-  // Append the time zone abbreviation. You can automate this with libraries like `moment-timezone`.
-  // For simplicity, here I'm just appending "CET", but do remember that Italy switches between CET and CEST.
-  formattedTime += " CET";
+  formattedTime += " (CET)";
 
   return formattedTime;
 }
 
 export function formatDate(date: Date): string {
-  return date.toLocaleDateString("en-US", {
+  return date.toLocaleDateString("DE", {
     year: "numeric",
     month: "long",
     day: "numeric",
